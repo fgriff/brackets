5cecd0f1a380476921da98893db0b659116775b6
fgriff <>
2021-03-02 21:26:25 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    const brackets = [];
+
+    bracketsConfig.forEach(item => brackets.push(item.join('')));
+
+    for (let i = 0; i < brackets.length; i++) {
+
+        if( str.includes(brackets[i]) ) {
+            str = str.replace(brackets[i], '');
+            i = -1;
+        }
+
+    }
+
+    return  !str;
 }
